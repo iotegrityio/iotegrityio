b4c1b7de8e9f988d5861dc52b5610a6d3006b354
Eren Türkay <eren@artistanbul.io>
2021-10-29 21:56:26 +0300

    README dışındaki dosyaları sil. Core reposunda takip ediliyor

--- ioauth/lib/ioauth/mailer.ex
+++
@@ -1,3 +0,0 @@
-defmodule Ioauth.Mailer do
-  use Swoosh.Mailer, otp_app: :ioauth
-end
--- ioauth/lib/ioauth/repo.ex
+++
@@ -1,5 +0,0 @@
-defmodule Ioauth.Repo do
-  use Ecto.Repo,
-    otp_app: :ioauth,
-    adapter: Ecto.Adapters.Postgres
-end
